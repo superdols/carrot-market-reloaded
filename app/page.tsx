51c3f12112a93a514a8a7a7f6c8422ef8f4c4f9d
superdols <superdols37@gmail.com>
2025-03-17 02:11:50 +0900

    form components

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,30 @@
-import Link from 'next/link';
+import FormButton from '@/components/form-btn';
+import FormInput from '@/components/form-input';
 
-export default function Home() {
+export default function CreateAccount() {
 	return (
-		<div className="flex flex-col items-center justify-between min-h-screen p-6">
-			<div className="my-auto flex flex-col items-center gap-2 *:font-medium">
-				<span className="text-9xl">🥕</span>
-				<h1 className="text-4xl ">당근</h1>
-				<h2 className="text-2xl">당근 마겟에 어서오세요!</h2>
+		<div className="flex flex-col gap-10 py-8 px-6">
+			<div className="flex flex-col gap-2 *:font-medium">
+				<h1 className="text-2xl">안녕하세요!</h1>
+				<h2 className="text-xl">Fill in the form below to join!</h2>
 			</div>
-			<div className="flex flex-col items-center gap-3 w-full">
-				<Link href="/create-account" className="primary-btn text-lg py-2.5">
-					시작하기
-				</Link>
-				<div className="flex gap-2">
-					<span>이미 계정이 있나요?</span>
-					<Link href="/login" className="hover:underline">
-						로그인
-					</Link>
-				</div>
-			</div>
+			<form className="flex flex-col gap-3">
+				<FormInput type="text" placeholder="Username" required errors={[]} />
+				<FormInput type="email" placeholder="Email" required errors={[]} />
+				<FormInput
+					type="password"
+					placeholder="Password"
+					required
+					errors={[]}
+				/>
+				<FormInput
+					type="password"
+					placeholder="Confirm Password"
+					required
+					errors={[]}
+				/>
+				<FormButton loading={false} text="Create account" />
+			</form>
 		</div>
 	);
 }
