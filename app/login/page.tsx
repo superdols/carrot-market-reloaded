06e6aaa805db31dc704ccfeecba665011ab5851c
superdols <superdols37@gmail.com>
2025-03-20 23:07:45 +0900

    server action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,35 @@
+'use client';
+
 import FormButton from '@/components/form-btn';
 import FormInput from '@/components/form-input';
 import SocialLogin from '@/components/social-login';
+import { handleForm } from './action';
+import { useActionState } from 'react';
 
 export default function LogIn() {
+	const [state, acton] = useActionState(handleForm, null);
 	return (
 		<div className="flex flex-col gap-10 py-8 px-6">
 			<div className="flex flex-col gap-2 *:font-medium">
 				<h1 className="text-2xl">안녕하세요!</h1>
 				<h2 className="text-xl">Log in with email and password.</h2>
 			</div>
-			<form className="flex flex-col gap-3">
-				<FormInput type="email" placeholder="Email" required errors={[]} />
+			<form action={acton} className="flex flex-col gap-3">
 				<FormInput
-					type="password"
-					placeholder="Password"
+					name="email"
+					type="email"
+					placeholder="Email"
 					required
 					errors={[]}
 				/>
-				<FormButton loading={false} text="Log in" />
+				<FormInput
+					name="password"
+					type="password"
+					placeholder="Password"
+					required
+					errors={state?.error ?? []}
+				/>
+				<FormButton text="Log in" />
 			</form>
 			<SocialLogin />
 		</div>
