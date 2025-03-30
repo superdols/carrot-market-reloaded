2f06fcbe4232f84d6247ed2aeab5b71de660e58a
superdols <superdols37@gmail.com>
2025-03-30 14:53:57 +0900

    authenticate fix

--- middleware.ts
+++ middleware.ts
@@ -15,8 +15,10 @@ const publicOnlyUrls: Routes = {
 export async function middleware(request: NextRequest) {
 	const session = await getSession();
 	const exists = publicOnlyUrls[request.nextUrl.pathname];
-	if (!session.id && !exists) {
-		return NextResponse.redirect(new URL('/', request.url));
+	if (!session.id) {
+		if (!exists) {
+			return NextResponse.redirect(new URL('/', request.url));
+		}
 	} else {
 		if (exists) {
 			return NextResponse.redirect(new URL('/profile', request.url));
