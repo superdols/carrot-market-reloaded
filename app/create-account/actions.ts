e1cad1ff88784decc3b0a1fabe224af7da11a933
superdols <superdols37@gmail.com>
2025-03-30 14:48:09 +0900

    authentication

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,10 +1,17 @@
 'use server';
+
 import {
 	PASSWORD_MIN_LENGTH,
 	PASSWORD_REGEX,
 	PASSWORD_REGEX_ERROR,
 } from '@/lib/constants';
+import db from '@/lib/db';
 import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import { getIronSession } from 'iron-session';
+import { sessions } from 'next/headers';
+import { redirect } from 'next/navigation';
+import getSession from '@/lib/session';
 
 const checkUsername = (username: string) => !username.includes('potato');
 
@@ -16,6 +23,30 @@ const checkPasswords = ({
 	confirmPassword: string;
 }) => password === confirmPassword;
 
+const checkUniqueUsername = async (username: string) => {
+	const user = await db.user.findUnique({
+		where: {
+			username: username,
+		},
+		select: {
+			id: true,
+		},
+	});
+	return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+	const user = await db.user.findUnique({
+		where: {
+			email: email,
+		},
+		select: {
+			id: true,
+		},
+	});
+	return !Boolean(user);
+};
+
 const formSchema = z
 	.object({
 		username: z
@@ -25,13 +56,56 @@ const formSchema = z
 			})
 			.toLowerCase()
 			.trim()
-			.refine(checkUsername, 'No potato allowed'),
-		email: z.string().email().toLowerCase(),
+			.refine(checkUsername, 'No potato allowed')
+			.refine(checkUniqueUsername, 'Username is already taken'),
+		email: z
+			.string()
+			.email()
+			.toLowerCase()
+			.refine(checkUniqueEmail, 'Email is already taken'),
 		password: z
 			.string()
 			.min(PASSWORD_MIN_LENGTH)
 			.regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 		confirmPassword: z.string().min(PASSWORD_MIN_LENGTH),
+	})
+	.superRefine(async ({ username }, ctx) => {
+		const user = await db.user.findUnique({
+			where: {
+				username: username,
+			},
+			select: {
+				id: true,
+			},
+		});
+		if (user) {
+			return ctx.addIssue({
+				code: 'custom',
+				message: 'Username is already taken',
+				path: ['username'],
+				fatal: true,
+			});
+			z.NEVER;
+		}
+	})
+	.superRefine(async ({ email }, ctx) => {
+		const user = await db.user.findUnique({
+			where: {
+				email,
+			},
+			select: {
+				id: true,
+			},
+		});
+		if (user) {
+			return ctx.addIssue({
+				code: 'custom',
+				message: 'email is already taken',
+				path: ['email'],
+				fatal: true,
+			});
+			z.NEVER;
+		}
 	})
 	.refine(checkPasswords, {
 		message: "Passwords don't match",
@@ -45,10 +119,24 @@ export async function createAccount(prevState: any, formData: FormData) {
 		password: formData.get('password'),
 		confirmPassword: formData.get('confirmPassword'),
 	};
-	const result = formSchema.safeParse(data);
+	const result = await formSchema.safeParseAsync(data);
 	if (!result.success) {
 		return result.error.flatten();
 	} else {
-		console.log(result.data);
+		const hashedPasswod = await bcrypt.hash(result.data.password, 12);
+		const user = await db.user.create({
+			data: {
+				username: result.data.username,
+				email: result.data.email,
+				password: hashedPasswod,
+			},
+			select: {
+				id: true,
+			},
+		});
+		const session = await getSession();
+		session.id = user.id;
+		await session.save();
+		redirect('/profile');
 	}
 }
---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+import getSession from './lib/session';
+
+interface Routes {
+	[key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+	'/': true,
+	'/login': true,
+	'/sms': true,
+	'/create-account': true,
+};
+
+export async function middleware(request: NextRequest) {
+	const session = await getSession();
+	const exists = publicOnlyUrls[request.nextUrl.pathname];
+	if (!session.id && !exists) {
+		return NextResponse.redirect(new URL('/', request.url));
+	} else {
+		if (exists) {
+			return NextResponse.redirect(new URL('/profile', request.url));
+		}
+	}
+}
+
+export const config = {
+	matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+};
